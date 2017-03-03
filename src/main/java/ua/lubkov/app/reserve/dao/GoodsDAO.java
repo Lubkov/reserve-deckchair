68b4cdb725fe94c223dc5edc3b0b451eb14e5c46
Lubkov <ser.lubkov@gmail.com>
2017-03-03 22:49:10 +0200

    создан интерфейс GoodsDAO

---
+++ src/main/java/ua/lubkov/app/reserve/dao/GoodsDAO.java
@@ -0,0 +1,14 @@
+package ua.lubkov.app.reserve.dao;
+
+import java.util.List;
+
+import ua.lubkov.app.reserve.domain.Goods;
+
+public interface GoodsDAO {
+	
+	public List<Goods> selectAll() throws Exception;
+	public Goods selectAt(Long id) throws Exception;
+	public Goods add(Goods item) throws Exception;
+	public void update(Goods item) throws Exception;
+	public void delete(Goods item) throws Exception;
+}
