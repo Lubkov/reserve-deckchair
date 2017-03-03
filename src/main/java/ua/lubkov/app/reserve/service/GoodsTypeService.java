f3531e0543e20fc291defb3ff91323bf23702b31
Lubkov <ser.lubkov@gmail.com>
2017-03-03 23:40:01 +0200

    исправлен GoodsTypeService

--- src/main/java/ua/lubkov/app/reserve/service/GoodsTypeService.java
+++ src/main/java/ua/lubkov/app/reserve/service/GoodsTypeService.java
@@ -1,5 +1,13 @@
 package ua.lubkov.app.reserve.service;
 
+import java.util.List;
+
+import ua.lubkov.app.reserve.domain.GoodsType;
+
 public interface GoodsTypeService {
 
+	public List<GoodsType> getAll() throws Exception;
+	public GoodsType getAt(Long id) throws Exception;
+	public GoodsType save(GoodsType item) throws Exception;
+	public void remove(GoodsType item) throws Exception;
 }
