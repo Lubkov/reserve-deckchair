e875a053cb3a08f6d5308a0619dbfba4acb94d06
Lubkov <ser.lubkov@gmail.com>
2017-03-03 23:46:51 +0200

    создан GoodsTypeServiceImpl

---
+++ src/main/java/ua/lubkov/app/reserve/service/GoodsTypeServiceImpl.java
@@ -0,0 +1,49 @@
+package ua.lubkov.app.reserve.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ua.lubkov.app.reserve.dao.GoodsTypeDAO;
+import ua.lubkov.app.reserve.domain.GoodsType;
+
+@Service("goodsType")
+public class GoodsTypeServiceImpl implements GoodsTypeService {
+	
+	@Autowired
+	private GoodsTypeDAO goodsTypeDAO;
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<GoodsType> getAll() throws Exception {
+
+		return goodsTypeDAO.selectAll();
+	}
+
+	@Override
+	public GoodsType getAt(Long id) throws Exception {
+
+		return goodsTypeDAO.selectAt(id);
+	}
+
+	@Override
+	public GoodsType save(GoodsType item) throws Exception {
+	
+		if (item.getId() == null) {
+			item = goodsTypeDAO.add(item);
+		} else {
+			goodsTypeDAO.update(item);
+		}
+			
+		return item;
+	}
+
+	@Override
+	public void remove(GoodsType item) throws Exception {
+
+		goodsTypeDAO.delete(item);
+	}
+
+}
