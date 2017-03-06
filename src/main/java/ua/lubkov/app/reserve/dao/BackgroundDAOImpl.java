81865c52f1adc334baf44414d8fe98e015a28035
Lubkov <ser.lubkov@gmail.com>
2017-03-06 21:05:56 +0200

    создан BackgroundDAOImpl

---
+++ src/main/java/ua/lubkov/app/reserve/dao/BackgroundDAOImpl.java
@@ -0,0 +1,39 @@
+package ua.lubkov.app.reserve.dao;
+
+import java.util.List;
+
+import ua.lubkov.app.reserve.domain.Background;
+
+public class BackgroundDAOImpl implements BackgroundDAO {
+
+	@Override
+	public List<Background> selectAll() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Background selectAt(Long id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Background add(Background item) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Background item) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Background item) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
